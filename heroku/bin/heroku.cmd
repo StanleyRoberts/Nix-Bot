ec988954e1c2b375928c070548c229f1b034a017
LordnistLost <Lordnsmaus@gmail.com>
2022-05-18 21:21:59 +0200

    something epic

---
+++ heroku/bin/heroku.cmd
@@ -0,0 +1,9 @@
+@echo off
+setlocal enableextensions
+
+set HEROKU_BINPATH=%~dp0\heroku.cmd
+if exist "%LOCALAPPDATA%\heroku\client\bin\heroku.cmd" (
+  "%LOCALAPPDATA%\heroku\client\bin\heroku.cmd" %*
+) else (
+  "%~dp0\..\client\bin\node.exe" "%~dp0\..\client\bin\run" %*
+)
