32473af45f710519af15b080704d5eab473197ca
LordnistLost <Lordnsmaus@gmail.com>
2022-07-02 01:10:42 +0200

    something cool

--- quotestBot.py
+++ quotestBot.py
@@ -5,6 +5,7 @@ import datetime
 from ctypes import sizeof
 import praw
 import random
+import prawcore
 
 
 TOKEN = 'OTc0NzY1NDI1Mjk4NjQ5MDk4.GGVRVj.r6Pzitvw3K0aTYKZTVR6s8mQwI-EvWq_t2GKiM'
@@ -13,6 +14,7 @@ CLIENT_ID = 'atOZU5hA9clYKG-oTZKNsA'
 SECRET_KEY = 'bchDQT8bPriqN03okgKNSgppYWGq2Q'
 USER_AGENT = 'atOZU5hA9clYKG-oTZKNsA'
 
+prefix = '!'
 
 intents = discord.Intents(messages=True, guilds=True, members=True)
 
@@ -73,50 +75,57 @@ async def on_message(msg):
         return
 
     if msg.channel.name == '🤖-bot-commands' or msg.channel.name == 'testing-of-the-bot🤖':
-        match user_message.lower():
-            case '!quote':
-                await msg.channel.send(give_quote().text)
-                
-            case '!annoystan':
-                await msg.channel.send('<@107523639410180096>')
-                
-            case '!activechange':
-                    guild = client.get_guild(msg.guild.id)
-                    mlist = memberlist(msg)
-                    for m in mlist:
-                        for i in guild.text_channels:
-                            messages = await i.history(limit=None, before=None, after=last_monday()).flatten()
-                            for j in messages:
-                                if j.author == guild.get_member(m[0]):
-                                    m[1] +=  1
-                        mem = guild.get_member(m[0])
-                        active = discord.utils.get(mem.guild.roles, name='active')
-                        Active = discord.utils.get(mem.guild.roles, name='Active')
-                        if m[1] < 11:
-                            if Active in mem.roles:
-                                await mem.remove_roles(Active)
-                                await mem.add_roles(active)                       
-                            elif active in mem.roles:
-                                await mem.remove_roles(active)
-                        else:
-                            if Active in mem.roles:
-                                continue
-                            elif active in mem.roles:
-                                await mem.remove_roles(active) 
-                                await mem.add_roles(Active)
+        message = user_message.lower().split()
+        if message[0] == prefix:
+            match message[1:]:
+                case 'quote':
+                    await msg.channel.send(give_quote().text)   
+                case 'annoystan':
+                    await msg.channel.send('<@107523639410180096>')
+                    
+                case 'activechange':
+                        guild = client.get_guild(msg.guild.id)
+                        mlist = memberlist(msg)
+                        for m in mlist:
+                            for i in guild.text_channels:
+                                messages = await i.history(limit=None, before=None, after=last_monday()).flatten()
+                                for j in messages:
+                                    if j.author == guild.get_member(m[0]):
+                                        m[1] +=  1
+                            mem = guild.get_member(m[0])
+                            active = discord.utils.get(mem.guild.roles, name='active')
+                            Active = discord.utils.get(mem.guild.roles, name='Active')
+                            if m[1] < 11:
+                                if Active in mem.roles:
+                                    await mem.remove_roles(Active)
+                                    await mem.add_roles(active)                       
+                                elif active in mem.roles:
+                                    await mem.remove_roles(active)
                             else:
-                                await mem.add_roles(Active)
-                    await msg.channel.send("done")
-        if '!meme' in user_message.lower().split():
-            messagew = user_message.split()
-            subr = messagew[1]
-            words = len(messagew)
-            if words >= 3:
-                time = messagew[2]
-            else:
-                time = "all"
-            fun = await getmeme(subr, time)
-            await msg.channel.send("***"+fun[0]+"***\n"+fun[1])
+                                if Active in mem.roles:
+                                    continue
+                                elif active in mem.roles:
+                                    await mem.remove_roles(active) 
+                                    await mem.add_roles(Active)
+                                else:
+                                    await mem.add_roles(Active)
+                        await msg.channel.send("done")
+                case 'meme':
+                    subr = message[1]
+                    words = len(message)
+                    if words >= 3:
+                        time = message[2]
+                    else:
+                        time = "all"
+                    try:
+                        fun = await getmeme(subr, time)
+                    except prawcore.exceptions.Redirect:
+                        await msg.channel.send("Subreddit\'"+subr+"\' not found")
+                    except prawcore.exceptions.NotFound:
+                        await msg.channel.send("Subreddit\'"+subr+"\' banned")
+                    except prawcore.exceptions.Forbidden:
+                        await msg.channel.send("Subreddit\'"+subr+"\' private")
+                    await msg.channel.send("***"+fun[0]+"***\n"+fun[1])
         return
 if __name__ == "__main__":     
     client.run(TOKEN)
