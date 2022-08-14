8a880bd88a8d95eb98d104c1f2c81d7bd2088448
StanleyRoberts <59836578+StanleyRoberts@users.noreply.github.com>
2022-08-14 23:57:27 +0100

    tidied code up significantly

--- quotestBot.py
+++ quotestBot.py
@@ -7,19 +7,15 @@ import praw
 import random
 import prawcore
 
+### CONSTANTS ###
 
 TOKEN = 'OTc0NzY1NDI1Mjk4NjQ5MDk4.GGVRVj.r6Pzitvw3K0aTYKZTVR6s8mQwI-EvWq_t2GKiM'
-
+API_KEY = 'PW28Q/V4PH0amKO/DbtbfQ==yR6f6tfRxigcL7uA'
 CLIENT_ID = 'atOZU5hA9clYKG-oTZKNsA'
 SECRET_KEY = 'bchDQT8bPriqN03okgKNSgppYWGq2Q'
 USER_AGENT = 'atOZU5hA9clYKG-oTZKNsA'
 
-prefix = '!'
-
-API_KEY = 'PW28Q/V4PH0amKO/DbtbfQ==yR6f6tfRxigcL7uA'
-
 intents = discord.Intents(messages=True, guilds=True, members=True)
-
 client = discord.Client(intents=intents)
 
 reddit_read_only = praw.Reddit(client_id = CLIENT_ID,
@@ -32,52 +28,60 @@ reddit_authorized = praw.Reddit(client_id = CLIENT_ID,
                                 username="WatchingRacoons",    
                                 password="M3m3s4Disc") 
 
-async def getmeme(subr, time):
-    subreddit = reddit_read_only.subreddit(subr)
-    memes = subreddit.top(time)
-    mem = []
-    for post in memes:
-        mem.append(post)
-    meme = mem[random.randint(0, len(mem)-1)]
-    if not meme.is_self:
-        link = meme.url
-    else:
+### Customisable variables ###
+
+prefix = '!'
+allowed_channels = ['🤖-bot-commands', 'testing-of-the-bot🤖']
+
+### Command Functions ###
+
+async def give_meme(message):
+    subr = message[1]
+    time = message[2] if len(message)>=3 else "all" # message[2] needs sanity check
+    try:
+        subreddit = await reddit_read_only.subreddit(subr)
+    except prawcore.exceptions.Redirect:
+        return "Subreddit\'"+subr+"\' not found"
+    except prawcore.exceptions.NotFound:
+        return "Subreddit\'"+subr+"\' banned"
+    except prawcore.exceptions.Forbidden:
+        return "Subreddit\'"+subr+"\' private"
+
+    posts = subreddit.top(time)
+    memes = [post for post in posts]
+    meme = random.choice(memes) # this should work directly with 'posts' variable but i cant test it
+
+    if meme.is_self:
         link = meme.selftext
-    return (meme.title, link)
-
-def last_monday():
-    today = datetime.datetime.today()
-    monday = today - datetime.timedelta(days=today.weekday())
-    return monday
-
-def memberlist(msg):
-    guild = client.get_guild(msg.guild.id)
-    mlist = []
-    for i in guild.members:
-        member_id = i.id
-        mlist.append([member_id, 0])
-    return mlist
+    else:
+        link = meme.url
+    return "***"+meme.title+"***\n"+link
 
 def give_fact():
     api_url = 'https://api.api-ninjas.com/v1/facts?limit={}'.format(1)
     response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
     if response.status_code == requests.codes.ok:
-        print(type(response.text))
         cjson = json.loads(response.text)
-        print(cjson[0]["fact"])
         return cjson[0]["fact"]
-    else:
-        print("Error:", response.status_code, response.text)
-    return "error"
+    return "Error: "+response.status_code+"\n"+response.text
 
+def give_quote():
+    response = requests.get("https://inspirobot.me/api?generate=true")
+    return response.text
+
+def parse_command(message):
+    match message[0][1:]:
+        case 'quote': return give_quote()
+        case 'annoystan': return '<@107523639410180096>'
+        case 'meme': return give_meme()
+        case 'fact': return give_fact()
+        case _: return "Unknown command"
+
+### Client Event Handlers ###
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
-def give_quote():
-    response = requests.get("https://inspirobot.me/api?generate=true")
-    return response
 
 @client.event
 async def on_message(msg):
@@ -89,61 +93,10 @@ async def on_message(msg):
     if msg.author == client.user:
         return
 
-    if msg.channel.name == '🤖-bot-commands' or msg.channel.name == 'testing-of-the-bot🤖':
-        message = user_message.lower().split()
-        if message[0][0] == prefix:
-            print("prefix here")
-            match message[0][1:]:
-                case 'quote':
-                    await msg.channel.send(give_quote().text)   
-                case 'annoystan':
-                    await msg.channel.send('<@107523639410180096>')
-                    
-                case 'activechange':
-                        guild = client.get_guild(msg.guild.id)
-                        mlist = memberlist(msg)
-                        for m in mlist:
-                            for i in guild.text_channels:
-                                messages = await i.history(limit=None, before=None, after=last_monday()).flatten()
-                                for j in messages:
-                                    if j.author == guild.get_member(m[0]):
-                                        m[1] +=  1
-                            mem = guild.get_member(m[0])
-                            active = discord.utils.get(mem.guild.roles, name='active')
-                            Active = discord.utils.get(mem.guild.roles, name='Active')
-                            if m[1] < 11:
-                                if Active in mem.roles:
-                                    await mem.remove_roles(Active)
-                                    await mem.add_roles(active)                       
-                                elif active in mem.roles:
-                                    await mem.remove_roles(active)
-                            else:
-                                if Active in mem.roles:
-                                    continue
-                                elif active in mem.roles:
-                                    await mem.remove_roles(active) 
-                                    await mem.add_roles(Active)
-                                else:
-                                    await mem.add_roles(Active)
-                        await msg.channel.send("done")
-                case 'meme':
-                    subr = message[1]
-                    words = len(message)
-                    if words >= 3:
-                        time = message[2]
-                    else:
-                        time = "all"
-                    try:
-                        fun = await getmeme(subr, time)
-                    except prawcore.exceptions.Redirect:
-                        await msg.channel.send("Subreddit\'"+subr+"\' not found")
-                    except prawcore.exceptions.NotFound:
-                        await msg.channel.send("Subreddit\'"+subr+"\' banned")
-                    except prawcore.exceptions.Forbidden:
-                        await msg.channel.send("Subreddit\'"+subr+"\' private")
-                    await msg.channel.send("***"+fun[0]+"***\n"+fun[1])                 
-                case 'fact':
-                    await msg.channel.send(give_fact())
-        return
+    message = user_message.lower().split()
+    if msg.channel.name in allowed_channels and message[0][0] == prefix:
+        response = parse_command(message)
+        await msg.channel.send(response)
+
 if __name__ == "__main__":     
     client.run(TOKEN)
