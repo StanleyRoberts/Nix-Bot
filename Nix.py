bf8c3be1daa21bc42b00b91eca51265f3ec61514
LordnistLost <Lordnsmaus@gmail.com>
2022-10-01 01:21:51 +0200

    Added a loser role for the person who messed up (something epic)

--- Nix.py
+++ Nix.py
@@ -75,13 +75,18 @@ async def get_highscore(ctx):
     highscore = cur.fetchall()
     await ctx.respond("Your highscore is {0}".format(highscore[0][0]))
     
-
+@bot.slash_command(name='set_loser_role', description="Set the role the person who failed at counting should get")
+@discord.commands.default_permissions(manage_guild=True)
+async def set_loserRole(ctx, role: discord.Role):
+    single_SQL("UPDATE Guilds SET RoleForLosers={0} WHERE ID={1}".format(role.id, ctx.guild_id))
+    await ctx.respond("The role for the loser is set to {0}".format(role.name))
+    
 @bot.event
 async def on_message(msg):
     if(msg.content.isdigit()):
         conn = sqlite3.connect("server_data.db")
         cur = conn.cursor()
-        cur.execute("SELECT CountingChannelID, CurrentCount, LastCounterID, HighScoreCounting FROM Guilds WHERE ID={0}".format(msg.guild.id))
+        cur.execute("SELECT CountingChannelID, CurrentCount, LastCounterID, HighScoreCounting, RoleForLosers FROM Guilds WHERE ID={0}".format(msg.guild.id))
         values = cur.fetchall()
         if(msg.channel.id == values[0][0]): #Checks for the right channel
             if(int(msg.content) == values[0][1] + 1): #Checks if it is the correct number
@@ -95,11 +100,12 @@ async def on_message(msg):
                     single_SQL("UPDATE Guilds SET CurrentCount = 0, LastCounterID = 0 WHERE ID = {0}".format(msg.guild.id))
                     await msg.add_reaction('<:NixCrying:1025433818527715459>')
                     await msg.channel.send("Counting failed: same user entered two numbers in a row")
+                    await msg.author.add_roles(msg.guild.get_role(values[0][4]))
             else: #Wrong number got typed in the chat
                 single_SQL("UPDATE Guilds SET CurrentCount = 0, LastCounterID = 0 WHERE ID = {0}".format(msg.guild.id))
                 await msg.add_reaction('<:NixCrying:1025433818527715459>')
                 await msg.channel.send("Counting failed: Wrong number")
-            
+                await msg.author.add_roles(msg.guild.get_role(values[0][4]))
             
 
 
