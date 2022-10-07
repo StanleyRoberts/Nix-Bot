541aecae49f2507f6b547b9107554cc54a6d6e6f
Stanley Roberts <stanley.heronwood+git@gmail.com>
2022-10-07 14:58:17 +0100

    refactor helpers

--- cogs/facts.py
+++ cogs/facts.py
@@ -1,26 +1,49 @@
 import discord
-from discord.ext import commands
-import functions.helpers as helper
+from discord.ext import commands, tasks
+import datetime as dt
+import functions.database as db
+import requests, json
+
+from Nix import API_KEY
 
 class Facts(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.daily_fact.start()
     
     @commands.slash_command(name='fact', description="Displays a random fact")
     async def send_fact(self, ctx):
-        await ctx.respond(helper.get_fact())
+        await ctx.respond(db.get_fact())
 
     @commands.slash_command(name='set_fact_channel', description="Sets the channel for daily facts")
     @discord.commands.default_permissions(manage_guild=True)
     async def set_fact_channel(self, ctx, channel: discord.TextChannel):
-        helper.single_SQL("UPDATE Guilds SET FactChannelID=%s WHERE ID=%s", (channel.id, ctx.guild_id))
+        db.single_SQL("UPDATE Guilds SET FactChannelID=%s WHERE ID=%s", (channel.id, ctx.guild_id))
         await ctx.respond("<:NixDrinking:1026494037043187713> Facts channel set to {0}".format(channel.mention), ephemeral=True)
 
     @commands.slash_command(name='stop_facts', description="Disables daily facts (run set_fact_channel to enable again)")
     @discord.commands.default_permissions(manage_guild=True)
     async def toggle_facts(self, ctx):
-        helper.single_SQL("UPDATE Guilds SET FactChannelID=NULL WHERE ID=%s", (ctx.guild_id,))
+        db.single_SQL("UPDATE Guilds SET FactChannelID=NULL WHERE ID=%s", (ctx.guild_id,))
         await ctx.respond("<:NixNoEmotion:1026494031670300773> Stopping daily facts", ephemeral=True)
+
+    @tasks.loop(time=dt.time(hour=9))
+    async def daily_fact(self):
+        guilds = db.single_SQL("SELECT FactChannelID FROM Guilds")
+        fact = db.get_fact()
+        for factID in guilds:
+            if factID[0]:
+                await (await self.bot.fetch_channel(factID[0])).send(fact)
+    
+    @staticmethod
+    def get_fact():
+        api_url = 'https://api.api-ninjas.com/v1/facts?limit={}'.format(1)
+        response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
+        message = "Error: "+str(response.status_code)+"\n"+response.text
+        if response.status_code == requests.codes.ok:
+            cjson = json.loads(response.text)
+            message = cjson[0]["fact"]
+        return message
 
 def setup(bot):
     bot.add_cog(Facts(bot))
--- cogs/reddit.py
+++ cogs/reddit.py
@@ -3,7 +3,7 @@ from discord.ext import commands, tasks
 import datetime as dt
 import asyncpraw as praw, asyncprawcore as prawcore
 import random
-import functions.helpers as helper
+from functions.database import single_SQL
 from Nix import CLIENT_ID, SECRET_KEY, USER_AGENT
 
 
@@ -13,6 +13,7 @@ class Reddit(commands.Cog):
         self.reddit = praw.Reddit(client_id = CLIENT_ID,
                             client_secret = SECRET_KEY, 
                             user_agent= USER_AGENT,)
+        self.daily_post.start()
     
     @commands.slash_command(name='reddit', description="Displays a random top reddit post from the given subreddit")
     async def send_reddit_post(self, ctx, subreddit,
@@ -21,35 +22,34 @@ class Reddit(commands.Cog):
                                                 description="Time period to search for top posts")):
         await ctx.respond(await self.get_reddit_post(subreddit, time))
 
-    @commands.slash_command(name='subscribe_to_subreddit', description="Get a daily meme from a subreddit")
+    @commands.slash_command(name='subscribe', description="Subscribe to a subreddit to get daily posts from it")
     @discord.commands.default_permissions(manage_guild=True)
-    async def subscribing(self, ctx, sub, channel : discord.Option(discord.TextChannel, required=False)):
+    async def subscribe_to_sub(self, ctx, sub, channel : discord.Option(discord.TextChannel, required=False)):
         if not channel:
             channel = ctx.channel
         try:
-            subr = await self.reddit.subreddit(sub)#Needed to check if subreddit exists
-            random.choice([post async for post in subr.top(time_filter="day", limit= 1)]) #Needed to check if subreddit exists
-            helper.single_SQL("INSERT INTO subreddits (GuildID, subreddit, SubredditChannelID) VALUES (%s, %s, %s)", (ctx.guild_id, sub, channel.id)) #Add subscription to SQL
+            (await self.reddit.subreddit(sub)).top(time_filter="day", limit= 1) #Needed to check if subreddit exists
+            single_SQL("INSERT INTO subreddits (GuildID, subreddit, SubredditChannelID) VALUES (%s, %s, %s)", (ctx.guild_id, sub, channel.id)) #Add subscription to SQL
             await ctx.respond("This server is now subscribed to {0}".format(sub))
          
         except prawcore.exceptions.AsyncPrawcoreException: #Can´t find subreddit exception
             await ctx.respond("The subreddit {0} is not available".format(sub))
         
-    @commands.slash_command(name='unsubscribe_from_subreddit', description="Do not get memes from the subreddit anymore")
+    @commands.slash_command(name='unsubscribe', description="Unsubscribe to daily posts from the given subreddit")
     @discord.commands.default_permissions(manage_guild=True)
-    async def unsubscribeFromSub(self, ctx, sub):
-        helper.single_SQL("DELETE FROM subreddits WHERE GuildID=%s AND subreddit=%s ", (ctx.guild_id, sub)) #Delete the subscription out of the SQL
+    async def unsubscribe_from_sub(self, ctx, sub):
+        single_SQL("DELETE FROM subreddits WHERE GuildID=%s AND subreddit=%s ", (ctx.guild_id, sub)) #Delete the subscription out of the SQL
         await ctx.respond("This server is now unsubscribed from {0}".format(sub))
         
     @commands.slash_command(name='test', description="Testing")
     async def testing(self, ctx):
-        subs = helper.single_SQL("SELECT GuildID, subreddit, SubredditChannelID FROM subreddits")
+        subs = single_SQL("SELECT GuildID, subreddit, SubredditChannelID FROM subreddits")
         for entry in subs:
             await (await self.bot.fetch_channel(entry[2])).send(await self.get_reddit_post(entry[1], "day"))
     
     @tasks.loop(time=dt.time(hour=9))
-    async def getSubbedMeme(self):
-        subs = helper.single_SQL("SELECT GuildID, subreddit, SubredditChannelID FROM subreddits")
+    async def daily_post(self):
+        subs = single_SQL("SELECT GuildID, subreddit, SubredditChannelID FROM subreddits")
         for entry in subs:
             self.bot.fetch_channel(entry[2]).send(self.get_reddit_post(entry[1], "day")) #Go through the SQL and post the requested post in the chosen channel
     
