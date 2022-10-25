9d49718e6e07e55cbd0a1d34bed2bad89b7be280
LordnistLost <Lordnsmaus@gmail.com>
2022-10-25 20:09:02 +0200

    Change the colour of the embed (something epic)

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -50,7 +50,7 @@ class Reddit(commands.Cog):
         db.single_SQL("DELETE FROM Subreddits WHERE GuildID=%s AND Subreddit=%s ",
                       (ctx.guild_id, sub))  # Delete the subscription out of the SQL
         await ctx.respond("This server is now unsubscribed from {0} <:NixSneaky:1033423327320080485>".format(sub))
-    
+
     @commands.slash_command(name='subscriptions', description="Get a list of the subscriptions of the server")
     async def getSubs(self, ctx):
         subscriptions = db.single_SQL("SELECT Subreddit FROM Subreddits WHERE GuildID=%s", (ctx.guild_id,))
@@ -61,10 +61,10 @@ class Reddit(commands.Cog):
             c += 1
         embed = discord.Embed(
             title="Subscription",
-            description = "These are the Subreddits you have subscribed to \n\n" + "\n".join(list),
-            color = discord.Colour.blurple())
+            description="These are the Subreddits you have subscribed to \n\n" + "\n".join(list),
+            color=discord.Colour.orange())
         await ctx.respond(embed=embed)
-        
+
     @tasks.loop(time=dt.time(hour=9))
     async def daily_post(self):
         """
