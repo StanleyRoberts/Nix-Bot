59fe05359c95cb6a020c6c2e9a304395d322ed70
LordnistLost <Lordnsmaus@gmail.com>
2022-11-20 01:50:22 +0100

    Make the help function into a new cog

--- cogs/help.py
+++ cogs/help.py
@@ -10,7 +10,7 @@ class Help(commands.Cog):
 
     @commands.slash_command(name='testing', description="just testing")
     async def button(self, ctx):
-        await ctx.respond("Look at this button", view=MyView())
+        await ctx.respond("Look at this button", view=BackwardButton())
 
     @commands.slash_command(name='help', description="Displays the help page for NixBot")
     async def display_help(self, ctx):
@@ -25,11 +25,26 @@ class Help(commands.Cog):
                               colour=Colours.PRIMARY)
         await ctx.respond(embed=embed)
 
+    @commands.slash_command(name='testinghelp', description="Testing for a better help function")
+    async def helper_embed(self, ctx):
+        desc = "Note: depending on your server settings and role permissions," + \
+            " some of these commands may be hidden or disabled\n\n***Generic***\n" \
+            + "".join(sorted([command.mention + " : " + command.description + "\n"
+                             for command in self.bot.walk_application_commands() if not command.cog]))
+        embed = discord.Embed(title="Help Page", description=desc,
+                              colour=Colours.PRIMARY)
+        await ctx.respond(embed=embed)
+        await (await self.bot.fetch_channel(ctx.channel.id)).send(view=BackwardButton())
 
-class MyView(discord.ui.View):
-    @discord.ui.button(label="Click me!", style=discord.ButtonStyle.primary)
+
+class BackwardButton(discord.ui.View):
+    @discord.ui.button(label="Backwards", style=discord.ButtonStyle.primary)
     async def button_callback(self, button, interaction):
-        await interaction.response.send_message("You clicked the button!")
+        await interaction.embed.add_field(name="testing", value="Hello this is a testing text", inline=True)
+
+    @discord.ui.button(label="Forwards", style=discord.ButtonStyle.primary)
+    async def button_callback(self, button, interaction):
+        await interaction.embed.add_field(name="testing", value="Hello this is a testing text", inline=True)
 
 
 def setup(bot):
