a764ad651e6ce63ca0dd1436b0745f6b85d9a1e7
Stanley Roberts <stanley.heronwood+git@gmail.com>
2022-11-20 03:11:35 +0000

    add interactive post viewer for /reddit command

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -1,11 +1,58 @@
 import discord
 import random
 from discord.ext import commands, tasks
+from discord.partial_emoji import PartialEmoji
 import asyncpraw as praw
 import asyncprawcore as prawcore
 import functions.database as db
 from functions.style import Emotes, Colours, TIME
 from Nix import CLIENT_ID, SECRET_KEY, USER_AGENT
+
+
+class ChangeSubModal(discord.ui.Modal):
+    """
+    Modal text box to change subreddit for post viewer
+
+    Args:
+        title (str): the modal title
+        time (str): the time period for reddit posts
+    """
+
+    def __init__(self, title, time):
+        super().__init__(title=title)
+        self.add_item(discord.ui.InputText(label="Subreddit"))
+        self.time = time
+
+    async def callback(self, interaction):
+        newsub = self.children[0].value
+        await interaction.response.edit_message(content=(await Reddit.get_reddit_post(newsub, self.time)),
+                                                view=PostViewer(newsub, self.time))
+
+
+class PostViewer(discord.ui.View):
+    """
+    Manages the PostViewer context, which interactively shows reddit posts
+
+    Args:
+        sub (str): the subreddit to show posts from
+        time (str): the time period to show posts from
+    """
+
+    def __init__(self, sub, time):
+        super().__init__()
+        self.sub = sub
+        self.time = time
+
+    @discord.ui.button(label="New Post", style=discord.ButtonStyle.primary,
+                       emoji=PartialEmoji.from_str(Emotes.BLEP))
+    async def refresh_callback(self, _, interaction):
+        await interaction.response.edit_message(content=(await Reddit.get_reddit_post(self.sub, self.time)),
+                                                view=PostViewer(self.sub, self.time))
+
+    @discord.ui.button(label="Change Subreddit", style=discord.ButtonStyle.secondary,
+                       emoji=PartialEmoji.from_str(Emotes.HUG))
+    async def change_sub_callback(self, _, interaction):
+        await interaction.response.send_modal(ChangeSubModal(title="Change Subreddit", time=self.time))
 
 
 class Reddit(commands.Cog):
@@ -23,7 +70,8 @@ class Reddit(commands.Cog):
                                                     choices=[
                                                         "month", "hour", "week", "all", "day", "year"],
                                                     description="Time period to search for top posts")):
-        await ctx.respond(await self.get_reddit_post(subreddit, time))
+        await ctx.interaction.response.send_message(await self.get_reddit_post(subreddit, time),
+                                                    view=PostViewer(subreddit, time))
 
     @commands.slash_command(name='subscribe', description="Subscribe to a subreddit to get daily posts from it")
     @discord.commands.default_permissions(manage_guild=True)
@@ -31,15 +79,12 @@ class Reddit(commands.Cog):
         if not channel:
             channel = ctx.channel
         try:
-            # Needed to check if subreddit exists
             [post async for post in (await self.reddit.subreddit(sub)).top(time_filter="day", limit=1)][0]
             db.single_SQL("INSERT INTO Subreddits (GuildID, Subreddit, SubredditChannelID) VALUES (%s, %s, %s)",
-                          (ctx.guild_id, sub, channel.id))  # Add subscription to SQL
+                          (ctx.guild_id, sub, channel.id))
             await ctx.respond("This server is now subscribed to {0} {1}".format(sub, Emotes.HUG))
-
-        except prawcore.exceptions.AsyncPrawcoreException:  # Can´t find subreddit exception
+        except prawcore.exceptions.AsyncPrawcoreException:
             await ctx.respond("The subreddit {0} is not available {1}}".format(sub, Emotes.EVIL))
-
         except db.KeyViolation:
             await ctx.respond("This server is already subscribed to {0} {1}".format(sub, Emotes.SUPRISE))
 
@@ -52,8 +97,8 @@ class Reddit(commands.Cog):
         if (sub,) not in db.single_SQL("SELECT Subreddit FROM Subreddits WHERE GuildID=%s", (ctx.guild_id,)):
             await ctx.respond("This server is not subscribed to r/{0} {1}".format(sub, Emotes.SUPRISE))
         else:
-            db.single_SQL("DELETE FROM Subreddits WHERE GuildID=%s AND Subreddit=%s ",
-                          (ctx.guild_id, sub))  # Delete the subscription out of the SQL
+            db.single_SQL(
+                "DELETE FROM Subreddits WHERE GuildID=%s AND Subreddit=%s ", (ctx.guild_id, sub))
             await ctx.respond("This server is now unsubscribed from {0} {1}".format(sub, Emotes.SNEAKY))
 
     @commands.slash_command(name='subscriptions', description="Get a list of the subscriptions of the server")
@@ -79,7 +124,6 @@ class Reddit(commands.Cog):
         subs = db.single_SQL(
             "SELECT GuildID, Subreddit, SubredditChannelID FROM Subreddits")
         for entry in subs:
-            # Go through the SQL and post the requested post in the chosen channel
             try:
                 await (await self.bot.fetch_channel(entry[2])).send("Daily post (" + entry[1] + ")\n" +
                                                                     (await self.get_reddit_post(entry[1], "day")))
@@ -90,7 +134,7 @@ class Reddit(commands.Cog):
     @staticmethod
     async def get_reddit_post(subreddit, time):
         """
-        Gets a random post (out of top 100) from a subreddit
+        Gets a random post (out of top 50) from a subreddit
 
         Args:
             subreddit (string): subreddit name to pull from
@@ -103,9 +147,10 @@ class Reddit(commands.Cog):
             Emotes.WTF, subreddit)
         try:
             subr = await Reddit.reddit.subreddit(subreddit)
-            subm = random.choice([post async for post in subr.top(time_filter=time, limit=100)])
+            subm = random.choice([post async for post in subr.top(time_filter=time, limit=50)])
             link = subm.selftext if subm.is_self else subm.url
-            response = "***" + subm.title + "***\n" + link
+            response = "***" + subm.title + \
+                "***\t(r/" + subreddit + ")\n" + link
         except prawcore.exceptions.Redirect:
             response = "{0} Subreddit \'".format(
                 Emotes.WTF) + subreddit + " \' not found"
