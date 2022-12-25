9ed1d72e87da8a3d5cc348e70a8948d2d54eb2b6
Stanley Roberts <stanley.heronwood+git@gmail.com>
2022-12-25 23:41:02 +0000

    swap to conversational model

--- cogs/nlp.py
+++ cogs/nlp.py
@@ -22,27 +22,20 @@ class NLP(commands.Cog):
             clean_prompt = re.sub(" @", " ",
                                   re.sub("@" + self.bot.user.name, "", msg.clean_content))
 
-            url = "https://api-inference.huggingface.co/models/bigscience/bloom"
+            url = "https://api-inference.huggingface.co/models/microsoft/DialoGPT-large"
             headers = {"Authorization": f"Bearer {HF_API}"}
 
-            prompt = "The following is a conversation with a phoenix named Nix. The phoenix is helpful, creative, " +\
-                "clever, and very friendly.\n\nHuman: Hello, who are you?\nNix: I am Nix, a phoenix made of fire. " +\
-                "How can I help you today?\nHuman: " + clean_prompt + "\nNix: "
+            prompt = {"past_user_inputs": ["What is your name?", "Who are you?"],
+                      "generated_responses": ["My name is Nix", "I am Nix, a phoenix made of flames"],
+                      "text": clean_prompt}
 
             data = json.dumps({"inputs": prompt, "parameters": {
                               "return_full_text": False, "temperature": 0.8,
-                              "use_cache": False, "repetition_penalty": 25,
-                              "max_new_tokens": 30}})
+                              "use_cache": False}})
             response = requests.request("POST", url, headers=headers, data=data)
 
-            text = json.loads(response.content.decode('utf-8'))[0]['generated_text']
-            print("\n\ngenerated text: " + text)
-
-            # strip non-Nix messages
-            trim = re.sub("Nix:", "", text[len(prompt):].split('Human: ')[0].split('\n')[0])
-
-            await msg.reply(trim if not any(ele in trim for ele in ['.', '!', '?', ')']) else "".join(
-                (re.findall('.*?[.!?)]', trim))))  # strip dangling sentences
+            text = json.loads(response.content.decode('utf-8'))
+            await msg.reply(text['generated_text'])
 
 
 def setup(bot):
