264306bfe6d3733cdedd96931a35a4e5710dc34a
LordnistLost <Lordnsmaus@gmail.com>
2023-01-28 21:56:53 +0100

    continue with charlatan (something epic)

--- cogs/charlatan.py
+++ cogs/charlatan.py
@@ -3,7 +3,7 @@ from discord.partial_emoji import PartialEmoji
 from functions.style import Emotes
 import discord
 from functions.style import Colours
-
+import random
 '''
 #TODO-List:
 -prep: players, choose word (own list or given list)
@@ -12,9 +12,21 @@ from functions.style import Colours
 -after: score of games(?), replay possible(start again without preparation)
 '''
 
-DEFAULT_WORDLIST = "Indiana Jones\nDr Who\nSpiderman\nDarth Vader\nSherlock Holmes\nGandalf"
-"\nSuperman\nBatman\nJames Bond\nDracula\nHomer Simpson\nFrankenstein"
-"\nRobin Hood\nSuper Mario\nTarzan\nDumbledore"
+DEFAULT_WORDLIST = """Indiana Jones\nDr Who\nSpiderman\nDarth Vader\nSherlock Holmes\nGandalf
+Superman\nBatman\nJames Bond\nDracula\nHomer Simpson\nFrankenstein
+Robin Hood\nSuper Mario\nTarzan\nDumbledore"""
+
+RULE_EXPLANATION = """ ```__Charlatan Rules__
+Every player is given the same list of words with the same word marked, except the *Charlatan* who gets an unmarked list of words.
+
+Each player takens it in turn to say a word related to the marked word (the *Charlatan* must try and guess a relevant word).
+
+A small amount of discussion time is permitted to try and identify the *Charlatan*.
+
+After discussion the players must vote on who they think is a *Charlatan*:
+- if they fail to eliminate the *Charlatan* they lose and the *Charlatan* scores 2pts.
+- if they eliminate the *Charlatan*, the *Charlatan* gets to guess the marked word. If they are correct the *Charlatan* gets 1pt and the other players get zero
+- if they eliminate the *Charlatan* and the *Chalartan* incorrectly guesses the marked word, the other players each get 1pt``` """
 
 
 class Charlatan(commands.Cog):
@@ -23,7 +35,7 @@ class Charlatan(commands.Cog):
 
     @commands.slash_command(name='charlatan', description="Play a game of Charlatan")
     async def start_game(self, ctx: discord.ApplicationContext):
-        user = ctx.author.mention
+        user = ctx.author
         view = CharlatanView(set([user]))
         await ctx.respond(embed=view.make_embed(), view=view)
 
@@ -31,7 +43,7 @@ class Charlatan(commands.Cog):
 class WordList(discord.ui.Modal):
     def __init__(self, title, users):
         super().__init__(title=title)
-        self.add_item(discord.ui.InputText(label="Add words for game here:",
+        self.add_item(discord.ui.InputText(label="Add 12 words for game here:",
                                            style=discord.InputTextStyle.long,
                                            placeholder=DEFAULT_WORDLIST))
         self.users = users
@@ -42,13 +54,13 @@ class WordList(discord.ui.Modal):
 
 
 class CharlatanView(discord.ui.View):
-    def __init__(self, users, wordlist=DEFAULT_WORDLIST):
+    def __init__(self, users, wordlist=DEFAULT_WORDLIST.split("\n")):
         super().__init__(timeout=300)
         self.users = users
         self.wordlist = wordlist
 
     def make_embed(self):
-        desc = "Playing right now: \n{}".format("\n".join(self.users))
+        desc = "Playing right now: \n{}".format("\n".join([user.mention for user in self.users]))
         return discord.Embed(title="Charlatan", description=desc,
                              colour=Colours.PRIMARY)
 
@@ -58,17 +70,23 @@ class CharlatanView(discord.ui.View):
 
     @discord.ui.button(label="Join", row=0, style=discord.ButtonStyle.primary)
     async def join_callback(self, _, interaction: discord.Interaction) -> None:
-        self.users.add(interaction.user.mention)
+        self.users.add(interaction.user)
         view = CharlatanView(self.users)
         await interaction.response.edit_message(embed=view.make_embed(), view=view)
 
     @discord.ui.button(label="Rules", row=1, style=discord.ButtonStyle.secondary)
     async def rules_callback(self, _, interaction: discord.Interaction) -> None:
-        return
+        await interaction.response.send_message(ephemeral=True, content=RULE_EXPLANATION)
 
     @discord.ui.button(label="Start Game", row=1, style=discord.ButtonStyle.primary)
-    async def start_callback(self, _, interaction):
-        return
+    async def start_callback(self, _, interaction: discord.Interaction):
+        await interaction.response.defer()
+        wordlist = self.wordlist[:12]  # For the case of the wordlist having more than 12 words
+        word = random.choice(wordlist)
+        charlatan = random.choice(list(self.users))
+        words = "\n".join([i if (i is not word) else "**" + i + "**" for i in wordlist])
+        for user in list(self.users):
+            await user.send(words) if not user == charlatan else await user.send("You are the charlatan: \n" + "\n".join(wordlist))
 
 
 def setup(bot: discord.Bot) -> None:
