23db3b27d968062ec3cc97f47ae0d8365eae500e
Stanley Roberts <stanley.heronwood+git@gmail.com>
2023-01-28 01:33:44 +0000

    add wordlist interaction

--- cogs/charlatan.py
+++ cogs/charlatan.py
@@ -12,31 +12,9 @@ from functions.style import Colours
 -after: score of games(?), replay possible(start again without preparation)
 '''
 
-
-class WordList(discord.ui.Modal):
-    def __init__(self, title):
-        super().__init__(title=title)
-        self.add_item(discord.ui.InputText(label="Add words for game here:",
-                                           style=discord.InputTextStyle.long,
-                      placeholder="London\nParis\nBerlin\nStockholm\nNew York\nCanberra"))
-
-    async def callback(self, interaction):
-        word_list = self.children[0].value.split('\n')
-        await interaction.message.edit(content=word_list)
-
-
-class CharlatanView(discord.ui.View):
-
-    def __init__(self):
-        super().__init__(timeout=300)
-
-    @discord.ui.button(label="Word List", style=discord.ButtonStyle.secondary)
-    async def callback(self, _, interaction):
-        await interaction.response.send_modal(WordList(title="Word List"))
-
-    @discord.ui.button(label="Start Game", style=discord.ButtonStyle.primary)
-    async def callback(self, _, interaction):
-        await interaction.response()
+DEFAULT_WORDLIST = "Indiana Jones\nDr Who\nSpiderman\nDarth Vader\nSherlock Holmes\nGandalf"
+"\nSuperman\nBatman\nJames Bond\nDracula\nHomer Simpson\nFrankenstein"
+"\nRobin Hood\nSuper Mario\nTarzan\nDumbledore"
 
 
 class Charlatan(commands.Cog):
@@ -50,25 +28,46 @@ class Charlatan(commands.Cog):
         await ctx.respond(embed=view.make_embed(), view=view)
 
 
-class CharlatanView(discord.ui.View):
+class WordList(discord.ui.Modal):
+    def __init__(self, title, users):
+        super().__init__(title=title)
+        self.add_item(discord.ui.InputText(label="Add words for game here:",
+                                           style=discord.InputTextStyle.long,
+                                           placeholder=DEFAULT_WORDLIST))
+        self.users = users
 
-    def __init__(self, users):
+    async def callback(self, interaction):
+        view = CharlatanView(set(self.users), self.children[0].value.split('\n'))
+        await interaction.message.edit(embed=view.make_embed(), view=view)
+
+
+class CharlatanView(discord.ui.View):
+    def __init__(self, users, wordlist=DEFAULT_WORDLIST):
         super().__init__(timeout=300)
         self.users = users
+        self.wordlist = wordlist
 
     def make_embed(self):
         desc = "Playing right now: \n{}".format("\n".join(self.users))
         return discord.Embed(title="Charlatan", description=desc,
                              colour=Colours.PRIMARY)
 
-    @discord.ui.button(label="join", style=discord.ButtonStyle.primary)
-    async def join_button(self, _, interaction: discord.Interaction) -> None:
+    @discord.ui.button(label="Word List", row=0, style=discord.ButtonStyle.secondary)
+    async def wordlist_callback(self, _, interaction):
+        await interaction.response.send_modal(WordList(title="Word List", users=self.users))
+
+    @discord.ui.button(label="Join", row=0, style=discord.ButtonStyle.primary)
+    async def join_callback(self, _, interaction: discord.Interaction) -> None:
         self.users.add(interaction.user.mention)
         view = CharlatanView(self.users)
         await interaction.response.edit_message(embed=view.make_embed(), view=view)
 
-    @discord.ui.button(label="rules", style=discord.ButtonStyle.primary)
-    async def rules(self, _, interaction: discord.Interaction) -> None:
+    @discord.ui.button(label="Rules", row=1, style=discord.ButtonStyle.secondary)
+    async def rules_callback(self, _, interaction: discord.Interaction) -> None:
+        return
+
+    @discord.ui.button(label="Start Game", row=1, style=discord.ButtonStyle.primary)
+    async def start_callback(self, _, interaction):
         return
 
 
