8a45aa7edbf8776e584a92b27c29dbf4aec6f593
Stanley Roberts <stanley.heronwood+git@gmail.com>
2023-02-24 12:11:05 +0000

    add ability for nix to track history in using replies

--- cogs/misc.py
+++ cogs/misc.py
@@ -12,10 +12,12 @@ from helpers.style import Emotes
 
 logger = Logger()
 
-USER_QS = ["Who are you?", "Is Stan cool?", "What is your favourite server?", "Where do you live?"]
+USER_QS = ["Who are you?", "Is Stan cool?", "What is your favourite server?", "Where do you live?",
+           "Unless I tell you what my name is, please call me \'fire friend\'"]
 NIX_AS = ["I am Nix, a phoenix made of flames", "Yes, I think Stan is the best!",
           "I love the Watching Racoons server the most!",
-          "I live in a volcano with my friends: DJ the Dragon and Sammy the Firebird."]
+          "I live in a volcano with my friends: DJ the Dragon and Sammy the Firebird.",
+          "Sure thing, fire friend!"]
 
 
 class Misc(commands.Cog):
@@ -54,16 +56,32 @@ class Misc(commands.Cog):
         Args:
             msg (discord.Message): Message that triggered event
         """
-        if (self.bot.user.mentioned_in(msg) and msg.reference is None):
+        if self.bot.user.mentioned_in(msg):
+            questions = USER_QS
+            answers = NIX_AS
+
+            inspect = msg
+            is_answer = True
+            while inspect.reference is not None:
+                if is_answer:
+                    answers.append(inspect.content)
+                else:
+                    questions.append(inspect.content)
+                inspect = inspect
+
+            if len(questions) != len(answers):
+                logger.error("Questions and Answers array for NLP chained reply are not the same size")
+                return
+
             logger.info("Generating AI response", member_id=msg.author.id, channel_id=msg.channel.id)
             clean_prompt = re.sub(" @", " ",
                                   re.sub("@" + self.bot.user.name, "", msg.clean_content))
 
-            url = "https://api-inference.huggingface.co/models/microsoft/DialoGPT-large"
+            url = "https://api-inference.huggingface.co/models/PygmalionAI/pygmalion-6b"
             headers = {"Authorization": f"Bearer {HF_API}"}
 
-            prompt = {"past_user_inputs": USER_QS,
-                      "generated_responses": NIX_AS,
+            prompt = {"past_user_inputs": questions,
+                      "generated_responses": answers,
                       "text": clean_prompt}
 
             data = json.dumps({"inputs": prompt,
@@ -73,8 +91,9 @@ class Misc(commands.Cog):
             response = requests.request("POST", url, headers=headers, data=data)
             if response.status_code != requests.codes.ok:
                 logger.error("{0} AI request failed: {1}".format(response.status_code, response.content))
-                msg.reply(
-                    "Uh-oh! I'm having trouble at the moment, please try again later {0}".format(Emotes.CONFUSED))
+                await msg.reply("Uh-oh! I'm having trouble at the moment, please try again later {0}"
+                                .format(Emotes.CONFUSED))
+                return
 
             text = json.loads(response.content.decode('utf-8'))
             await msg.reply(text['generated_text'])
