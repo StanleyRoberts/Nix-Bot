31d4bee77301b515d66347dc2b415b11c1f2f813
LordnistLost <lordnsmaus@gmail.com>
2023-09-12 16:10:23 +0200

    Take out unused variables (s.e.-FBL)

--- src/helpers/env.py
+++ src/helpers/env.py
@@ -12,7 +12,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')  # PRAW/Reddit API secret key
 USER_AGENT = os.getenv('USER_AGENT')  # PRAW/Reddit API user agent
 NINJA_API_KEY = os.getenv('NINJA_API_KEY')  # X-API-Key for API-Ninjas
 DATABASE_URL = os.getenv('DATABASE_URL')  # PostgreSQL db
-HF_API = os.getenv('HF_API')  # HuggingFace API key
 CAI_CHAR_TOKEN = os.getenv('CAI_CHAR_TOKEN')  # Character AI client token
 CAI_NIX_ID = os.getenv('CAI_NIX_ID')  # Character AI character ID of Nix bot
 
@@ -39,5 +38,7 @@ if NINJA_API_KEY is None:
     logger.error("NINJA_API_KEY environment variable missing")
 if DATABASE_URL is None:
     logger.error("DATABASE_URL environment variable missing")
-if HF_API is None:
-    logger.error("HF_API environment variable missing")
+if CAI_CHAR_TOKEN is None:
+    logger.error("CAI_CHAR_TOKEN environment variable missing")
+if CAI_NIX_ID is None:
+    logger.error("CAI_NIX_ID environment variable missing")
