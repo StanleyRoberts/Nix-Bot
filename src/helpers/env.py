e6f778bb07cbb1c7fa0ac4e071332522d5688578
Stanley Roberts <stanley.heronwood+git@gmail.com>
2023-09-16 14:07:40 +0100

    add action for staging. tests requirements and environment vars

--- src/helpers/env.py
+++ src/helpers/env.py
@@ -6,14 +6,26 @@ if __debug__:
 
 logger = Logger()
 
-TOKEN = os.getenv('TOKEN')  # Discord Token
-CLIENT_ID = os.getenv('CLIENT_ID')  # PRAW/Reddit API client ID
-SECRET_KEY = os.getenv('SECRET_KEY')  # PRAW/Reddit API secret key
-USER_AGENT = os.getenv('USER_AGENT')  # PRAW/Reddit API user agent
-NINJA_API_KEY = os.getenv('NINJA_API_KEY')  # X-API-Key for API-Ninjas
-DATABASE_URL = os.getenv('DATABASE_URL')  # PostgreSQL db
-HF_API = os.getenv('HF_API')  # HuggingFace API key
+registered = set()
 
+
+def load_env(name: str) -> str:
+    env = os.getenv('TOKEN')
+    if not env:
+        logger.error(f"{name} environment variable missing")
+    if __debug__:
+        global registered
+        registered.add(name)
+    return env
+
+
+TOKEN = load_env('TOKEN')  # Discord Token
+CLIENT_ID = load_env('CLIENT_ID')  # PRAW/Reddit API client ID
+SECRET_KEY = load_env('SECRET_KEY')  # PRAW/Reddit API secret key
+USER_AGENT = load_env('USER_AGENT')  # PRAW/Reddit API user agent
+NINJA_API_KEY = load_env('NINJA_API_KEY')  # X-API-Key for API-Ninjas
+DATABASE_URL = load_env('DATABASE_URL')  # PostgreSQL db
+HF_API = load_env('HF_API')  # HuggingFace API key
 
 if __debug__:
     import testing.postgresql as tp  # type: ignore[import]
@@ -24,19 +36,3 @@ if __debug__:
 def shutdown_db() -> None:
     if __debug__:
         postgres.stop()
-
-
-if TOKEN is None:
-    logger.error("TOKEN environment variable missing")
-if CLIENT_ID is None:
-    logger.error("CLIENT_ID environment variable missing")
-if SECRET_KEY is None:
-    logger.error("SECRET_KEY environment variable missing")
-if USER_AGENT is None:
-    logger.error("USER_AGENT environment variable missing")
-if NINJA_API_KEY is None:
-    logger.error("NINJA_API_KEY environment variable missing")
-if DATABASE_URL is None:
-    logger.error("DATABASE_URL environment variable missing")
-if HF_API is None:
-    logger.error("HF_API environment variable missing")
